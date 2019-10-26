60ce412f868000da20d5ac844a90b688f3fc547a
Anni <anni@winnersjo.se>
2019-10-26 15:00:29 +0200

    adding stuff

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    </div> -->
+    <the-header />
     <router-view />
   </div>
 </template>
+<script>
+import TheHeader from './components/TheHeader'
+ export default {
+   components: {
+     TheHeader
+ } 
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -16,7 +26,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+      <div id="logo-container">
+        <router-link to="/" ><img src="cupcakes & cookies.png" />Logo</router-link>
+      </div>
+      <div id="header-links-container">
+        <div class="pink-border-box">
+          <router-link to="/" ><img src="house.png" class="my-icon" />House</router-link>
+        </div>
+        <div class="pink-border-box">
+          <router-link to="/" ><img src="compass-arrow.png" class="my-icon" />arrow</router-link>
+        </div>
+        <div class="pink-border-box">
+          <img src="file.png" class="my-icon" />
+        </div>
+        <div class="pink-border-box">
+          <router-link to="/" ><img src="question-mark.png" class="my-icon" />Question mark</router-link>
+        </div>
+        <div class="pink-border-box">
+          <router-link to="/" ><img src="person.png" class="my-icon" />Person</router-link>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'TheHeader'
+}
+</script>
+
+<style>
+
+</style>
